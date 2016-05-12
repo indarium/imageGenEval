6edf61b3a6a4c58875e85118487bb97a18af0f81
cvandrei <crille@indarium.de>
2016-05-12 11:41:49 +0200

    javadoc and unit tests for StringUtil

--- src/main/java/util/StringUtil.java
+++ src/main/java/util/StringUtil.java
@@ -2,27 +2,64 @@ package util;
 
 /**
  * The padding code has been copied from: https://stackoverflow.com/questions/388461/how-can-i-pad-a-string-in-java#388476
- *
+ * <p>
  * author: cvandrei
  * since: 2016-05-11
  */
 public final class StringUtil {
 
-    private StringUtil() {}
+    private StringUtil() {
+    }
 
-    public static String padRight(String s, int n) {
+    /**
+     * Pad right with character '0'. If the input string is as long (or longer) than the desired length the original
+     * string is returned.
+     *
+     * @param s not null; the string to pad right
+     * @param n desired length
+     * @return never null
+     */
+    public static String padRight(final String s, final int n) {
         return padRight(s, n, '0');
     }
 
-    public static String padRight(String s, int n, char paddingChar) {
+    /**
+     * Pad right with the specified paddingChar. If the input string is as long (or longer) than the desired length the original
+     * string is returned.
+     *
+     * @param s           not null; the string to pad right
+     * @param n           desired length
+     * @param paddingChar character to pad with
+     * @return never null
+     */
+    public static String padRight(final String s, final int n, final char paddingChar) {
+        if (s == null) throw new IllegalArgumentException("s may not be null");
         return String.format("%1$-" + n + "s", s).replace(' ', paddingChar);
     }
 
-    public static String padLeft(String s, int n) {
+    /**
+     * Pad left with character '0'. If the input string is as long (or longer) than the desired length the original
+     * string is returned.
+     *
+     * @param s not null; the string to pad left
+     * @param n desired length
+     * @return never null
+     */
+    public static String padLeft(final String s, final int n) {
         return padLeft(s, n, '0');
     }
 
-    public static String padLeft(String s, int n, char paddingChar) {
+    /**
+     * Pad left with the specified paddingChar. If the input string is as long (or longer) than the desired length the original
+     * string is returned.
+     *
+     * @param s           not null; the string to pad left
+     * @param n           desired length
+     * @param paddingChar character to pad with
+     * @return never null
+     */
+    public static String padLeft(final String s, final int n, final char paddingChar) {
+        if (s == null) throw new IllegalArgumentException("s may not be null");
         return String.format("%1$" + n + "s", s).replace(' ', paddingChar);
     }
 
